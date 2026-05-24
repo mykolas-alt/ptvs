6ee058b1ba6c582da7806df5d5b42fa21f294b44
Cryptic5 <84860494+Cryptic5@users.noreply.github.com>
2026-05-24 22:57:59 +0300

    added a condition to scheduler

--- server/ptvs-rest-service/src/main/java/lt/pskurimas/ptvs/scheduler/NotificationScheduler.java
+++ server/ptvs-rest-service/src/main/java/lt/pskurimas/ptvs/scheduler/NotificationScheduler.java
@@ -3,6 +3,7 @@ package lt.pskurimas.ptvs.scheduler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import lt.pskurimas.ptvs.service.NotificationSendingService;
@@ -10,6 +11,7 @@ import lt.pskurimas.ptvs.service.NotificationSendingService;
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "ptvs.notifications.enabled", havingValue = "true", matchIfMissing = true)
 public class NotificationScheduler {
 
     private final NotificationSendingService notificationSendingService;
